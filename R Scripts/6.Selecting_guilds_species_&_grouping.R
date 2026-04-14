e6c1e2ec5853214a2094de613b5d0c8f0a356ab3
jasminefreeman <jasmine.freeman@strath.ac.uk>
2026-04-14 12:30:45 +0100

    Updated to include a season column

--- R Scripts/6.Selecting_guilds_species_&_grouping.R
+++ R Scripts/6.Selecting_guilds_species_&_grouping.R
@@ -14,12 +14,19 @@ data <- readRDS(paste0("./Objects/6.All_data__base_exp_&_diff_", region_name, ".
 data_small <- data %>% 
   select(
     region, hw_month, hw_temp, time, year, yearday, hw_start.x, hw_end.x,
-    matches("^phyt|omni|carn|fishp|bird"))
+    matches("^phyt|omni|carn|fishp|bird")) %>% 
+  mutate(season = case_when(
+    hw_month %in% c(12, 1, 2) ~ "Winter",
+    hw_month %in% c(3, 4, 5) ~ "Spring",
+    hw_month %in% c(6, 7, 8) ~ "Summer", 
+    hw_month %in% c(9, 10, 11) ~ "Autumn")) %>% 
+  mutate(season = factor(season, 
+                         levels = c("Spring", "Summer", "Autumn", "Winter")))
 
 # mutating data to stack all guilds in one column
 # also grouping so that there is no inshore/offshore values. They just combine for a 'total'
 data_grouped <- data_small %>% 
-  pivot_longer(cols = -c(region, hw_month, hw_temp, time, year, yearday, hw_start.x, hw_end.x),
+  pivot_longer(cols = -c(region, hw_month, hw_temp, time, year, yearday, hw_start.x, hw_end.x, season),
                names_to = "Guild",
                values_to = "Values") %>% 
   mutate(
@@ -34,7 +41,7 @@ data_grouped <- data_small %>%
       grepl("_diff$", Guild) ~ "diff",
       grepl("_base$", Guild) ~ "base",
       T ~ "experiment")) %>% 
-  group_by(region, hw_month, hw_temp, time, year, yearday, guild_group, col_type) %>% 
+  group_by(region, hw_month, hw_temp, time, year, yearday, hw_start.x, hw_end.x, season, guild_group, col_type) %>% 
   summarise(Values = sum(Values, na.rm = T), .groups = "drop")
   
 
